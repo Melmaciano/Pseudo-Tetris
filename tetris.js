7d6a82c3fd6705e1274e28b1a07c10ee3d2c1caf
Melmaciano <181913905+Melmaciano@users.noreply.github.com>
2024-11-27 16:52:40 -0300

    finish game

--- tetris.js
+++ tetris.js
@@ -1,6 +1,6 @@
 // GENERAL FUNCTIONS
-const makeLine = (length, char = "~") => Array.from({ length }, _ => char);      // Call twice to make grid
-const random = (num) => Math.trunc(Math.random() * num);
+const makeLine = (length, char = " ~ ") => Array.from({ length }, _ => char);      // Call twice to make grid
+const random = (num) => Math.trunc(Math.random()  *  num);
 const deepCopy = (arr) => {
     if (!Array.isArray(arr)) return arr;
     else return arr.map(subArr => deepCopy(subArr));
@@ -59,7 +59,10 @@ class Figure {
         this.allCoords = deepCopy(coords);
         this.currentIndexPosition = random(this.allCoords.length);
         this.currentPosition = deepCopy(this.allCoords[this.currentIndexPosition]);
-        this.currentPosition.forEach(square => square[1] += distanceFromBorder);
+        this.currentPosition.forEach(square => {
+            square[1] += distanceFromBorder;
+            square[0] -= 3;
+        });
     }
 
     updatePosition() {
@@ -99,38 +102,44 @@ class Game {
         this.col = col;
         this.grid = makeLine(row).map(_ => makeLine(col));
         this.ms = ms;
+        this.points = 0;
+        this.currentFIgure;
     }
 
     start() {
-        figure = new Figure(figures[random(figures.length)], Math.trunc(this.col / 2));      
+        DOM.updatePoints(this.points);
+        this.currentFIgure = new Figure(figures[random(figures.length)], Math.trunc(this.col / 2));      
 
         const timerID = setInterval(() => {           //  INTERVAL FOR THE RHYTHM OF GAME, ENSURE PIECES GO DOWN
-            let bool = this.updateGrid(figure);
-            if (bool) {                               // WHEN A COLLISION IS DETECTED INTERVAL ENDS AND A NEW PIECE
-                clearInterval(timerID);               // ENTER
-                this.start() 
-            };
             DOM.printDOM(this.grid);
+            let value = this.updateGrid(this.currentFIgure);
+            if (value === 1) {                               // WHEN A COLLISION IS DETECTED INTERVAL ENDS AND A NEW PIECE
+                clearInterval(timerID);                      // ENTER
+                this.start() 
+            } else if (value === 2) {
+                this.points = 0;
+                clearInterval(timerID);
+                DOM.gameOver();
+            }
         }, this.ms);
     }
 
     executeAction(key) {
         switch (key) {
             case "ArrowLeft":
-                this.updateGrid(figure, "left");
+                this.updateGrid(this.currentFIgure, "left");
                 break;
             case "ArrowRight":
-                this.updateGrid(figure, "right");
+                this.updateGrid(this.currentFIgure, "right");
                 break;
             case "ArrowUp":
-                this.updateGrid(figure, "up");
+                this.updateGrid(this.currentFIgure, "up");
                 break;
         }
     }
 
     // KEY FUNCTION, ENSURE RHYTHM OF GAME AND UPDATE POSITION OF PIECES, EITHER VERTICAL OR HORIZONTAL
     updateGrid(figure, direction) {
-        this.checkFullRow();
         const oldCoords = deepCopy(figure.currentPosition); 
 
         switch (direction) {
@@ -154,20 +163,22 @@ class Game {
             figure.currentPosition = oldCoords;
 
             if (!direction) { 
-                this.placeFigure(figure.currentPosition); 
-                return true; 
+                if (this.checkGameOver(figure)) return 2;   // GAME OVER
+                this.placeFigure(figure)
+                this.checkFullRow();
+                return 1;                                   // NEW FIGURE
             } else {
-                return false;
+                return 0;                                   // NEXT FRAME
             }
         } else {
-            oldCoords.forEach(square => this.updateCell(square, "~"));
-            newCoords.forEach(square => this.updateCell(square, "*"));
+            oldCoords.forEach(square => this.updateCell(square, " ~ "));
+            newCoords.forEach(square => this.updateCell(square, " $ "));
         }
     }
 
     checkCollision(figure) {
         for (const square of figure.currentPosition) {
-            if (this.grid?.[square[1]]?.[square[0]] === "#" ||
+            if (this.grid?.[square[0]]?.[square[1]] === " # " ||
                 square[1] < 0                               ||
                 square[1] > this.grid[0].length - 1         ||
                 square[0] === this.grid.length)
@@ -180,87 +191,133 @@ class Game {
 
     updateCell([ positionY, positionX ], char) {
         if (positionY < 0) return;
-        this.grid[positionX][positionY] = char;
+        this.grid[positionY][positionX] = char;
     }
 
-    placeFigure(arrOfSquares) {
-        arrOfSquares.forEach(square => this.updateCell(square, "#"));
+    placeFigure(figure) {
+        figure.currentPosition.forEach(square => this.updateCell(square, " # "));
     }
 
     // REMOVE FULL ROWS
-    checkFullRow() {
-        for (let i = this.grid.length - 1; i >= 0; i--) {
-            if (this.checkRow(this.grid[i])) this.shiftAllRows(i);
+
+    shiftAllRows(pos) {
+        for (let row = pos; row > 0; row--) {
+            this.grid[row] = this.grid[row - 1];
+            this.grid[0] = makeLine(this.row);
         }
-        console.log(true)
     }
 
     checkRow(row) {
-        return row.every(elem => elem === "#") ? true : false;
+        return row.every(elem => elem === " # ") ? true : false;
     }
 
-    shiftAllRows(pos) {
-        for (col of this.grid) {
-            for (let row = pos; row > 0; row--) {
-                if (col[row] !== "*" && col[row - 1] !== "*") col[row] = col[row - 1];
+    checkFullRow() {
+        for (let i = this.grid.length - 1; i >= 0; i--) {
+            if (this.checkRow(this.grid[i])) { 
+                this.shiftAllRows(i); 
+                i++;
+                this.addPoints();
             }
         }
+    }
+
+    // POINTS
+    addPoints() {
+        this.points += 100;
+        DOM.updatePoints(this.points);
+    }
+
+    // GAME OVER
+    checkGameOver(figure) {
+        return figure.currentPosition.some(square => (square[0] < 0) || (square[1] < 0));
     }
 }
 
 // DOM
 class DOMmanipulator {
     constructor() {
+        const body = document.querySelector("body");
+
+        this.colsInput = document.createElement("input");
+        this.cols = document.createElement("p");
+        this.cols.textContent = "Columns: ";
+        this.cols.appendChild(this.colsInput);
+        this.colsInput.value = 15;
+        this.colsInput.setAttribute("type", "number");
+
+        this.rowsInput = document.createElement("input");
+        this.rows = document.createElement("p");
+        this.rows.textContent = "Rows: ";
+        this.rows.appendChild(this.rowsInput);
+        this.rowsInput.value = 10;
+        this.rowsInput.setAttribute("type", "number");
+
+        this.options = document.createElement("div");
+        this.options.appendChild(this.cols);
+        this.options.appendChild(this.rows);
+        body.appendChild(this.options);
+
         this.startBtn = document.createElement("button");
         this.startBtn.classList.add("btn");
         this.startBtn.textContent = "start";
-        this.startBtn.addEventListener("click", this.startGame);
-        document.querySelector("body").appendChild(this.startBtn);
+        this.startBtn.addEventListener("click", this.startGame.bind(this));
+        body.appendChild(this.startBtn);
+
+        this.pointsPara = document.createElement("p");
+        this.pointsWord = document.createTextNode("Points: ");
+        this.pointsScore = document.createElement("span");
+        this.pointsPara.appendChild(this.pointsWord);
+        this.pointsPara.appendChild(this.pointsScore);
+        body.appendChild(this.pointsPara);
 
         this.gridDOM = document.createElement("div");
         this.gridDOM.classList.add("grid");
-        document.querySelector("body").appendChild(this.gridDOM);
+        body.appendChild(this.gridDOM);
+
+        this.gameOverMsg = document.createElement("p")
+        this.gameOverMsg.textContent = "GAME OVER";
+        this.gameOverMsg.classList.add("hidden");
+        body.appendChild(this.gameOverMsg);
     }
 
     startGame() {
-        const rows = 10, cols = 10, ms = 500;
-        game = new Game(ms, rows, cols);
+        this.gameOverMsg.classList.add("hidden");
+        const ms = 400;
+        game = new Game(ms, this.colsInput.value, this.rowsInput.value);
         game.start();
     }
 
     printDOM(grid) {
-        let formattedString = "";
-        const colLength = grid[0].length
+        this.gridDOM.textContent = grid.reduce((acc, row) => acc + (row.join(" ") + "\n"), "");
+    }
 
-        for(let row = 0; row < colLength; row++) {
+    updatePoints(points) {
+        this.pointsScore.textContent = points;
+    }
 
-            for(let col of grid) {
-                formattedString += col[row];
-            }
-
-            formattedString += "\n";
-        }
-
-        this.gridDOM.textContent = formattedString;
+    gameOver() {
+        this.gridDOM.textContent = "";
+        this.gameOverMsg.classList.remove("hidden");
+        this.pointsScore.textContent = 0;
+        game = "";
     }
 }
 
 // GLOBAL VARIABLES 
 const DOM = new DOMmanipulator;
-let game, figure;
+let game;
 let timerKey, arrowKeyPressed;
 
-// ENSURE AT LEAST KEY IS PRESSED AT LEAST ONE TIME, THEN IF THE KEY
-// IS PRESSED, THE INTERVAL CALL FUNCTION EVERY MS TIME
+// ENSURE A KEY IS PRESSED AT LEAST ONE TIME, THEN IF THE KEY
+// IS PRESSED, THE INTERVAL CALL THE FUNCTION EVERY MS TIME
 addEventListener("keydown", (e) => {
     if (!arrowKeyPressed && (e.key === "ArrowRight" || e.key === "ArrowLeft")) { 
         game.executeAction(e.key);
+        arrowKeyPressed = true;
 
         timerKey = setInterval(() => {
             game.executeAction(e.key);
         }, 300);
-
-        arrowKeyPressed = true;
     }
 });
 addEventListener("keyup", (e) => {
