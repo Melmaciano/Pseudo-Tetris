5514dddb24b00d609d5ab4f04e8fa054983286a5
Melmaciano <181913905+Melmaciano@users.noreply.github.com>
2024-11-26 22:26:49 -0300

    estaba mejor antes

--- tetris.js
+++ tetris.js
@@ -20,54 +20,58 @@ const figures = [
         name: "ele",
         coords: [
             [[0,0], [1,0], [0,1], [0,2]],
-            [[0,0], [0,1], [1,1], [2,1]],
+            [[0,1], [0,0], [1,1], [2,1]],
+            [[1,2], [0,2], [1,1], [1,0]],
+            [[2,1], [2,2], [1,1], [0,1]],
         ],
     },
     {
         id: 2,
         name: "stick",
         coords: [
-            [[0,0], [0,1], [0,2], [0,3]],
-            [[-1,1], [0,1], [1,1], [2,1]],
+            [[0,0], [1,0], [2,0], [3,0]],
+            [[1,-1], [1,0], [1,1], [1,2]],
         ],
     },
     {
         id: 3,
         name: "zeta",
         coords: [
-            [[0,1], [1,1], [1, 0], [2,0]],
-            [[0,0], [0,1], [1,1], [1,2]],
+            [[1,0], [1,1], [0,1], [0,2]],
+            [[0,0], [1,0], [1,1], [2,1]],
         ]
     },
     {
         id: 4,
         name: "clover",
         coords: [
-            [[0,1], [1,1], [1,0], [2,1]],
-            [[1,0], [1,1], [1,2], [2,1]],
+            [[1,0], [1,1], [0,1], [1,2]],
+            [[0,1], [1,1], [1,2], [2,1]],
+            [[0,0], [0,1], [1,1], [0,2]],
+            [[0,1], [1,1], [1,0], [2,1]]
         ]
     }
 ];
 
 // FIGURE
 class Figure {
-    constructor({ id, coords }) {
-        this.figure = id;
+    constructor({ coords }, distanceFromBorder) {
         this.allCoords = deepCopy(coords);
         this.currentIndexPosition = random(this.allCoords.length);
         this.currentPosition = deepCopy(this.allCoords[this.currentIndexPosition]);
+        this.currentPosition.forEach(square => square[1] += distanceFromBorder);
     }
 
     updatePosition() {
-        this.currentPosition.forEach(square => square[1]);
+        this.currentPosition.forEach(square => square[0]++);
     }
 
     moveRight() {
-        this.currentPosition.forEach(square => square[0]);
+        this.currentPosition.forEach(square => square[1]++);
     }
 
     moveLeft() {
-        this.currentPosition.forEach(square => square[0]);
+        this.currentPosition.forEach(square => square[1]--);
     }
 
     spin() { 
@@ -86,29 +90,19 @@ class Figure {
             square[1] += differenceY;
         });
     }
-
-    // // CHECK IF THE FIGURE IS NOT OUTSIDE THE BOARD
-    // static checkOutsideBoard({ coords: { square1: { positionX: a }, square2: { positionX: b }, 
-    //                                      square3: { positionX: c }, square4: { positionX: d } } }, dir) {
-    //     if (dir === "left") {
-    //         return Math.min(a, b, c, d) - 1 < 0;
-    //     } else {
-    //         return Math.max(a, b, c, d) + 1 > game.grid[0].length - 1;
-    //     }
-    // }
 }
-
-const a = new Figure(figures[1])
 
 // GAME
 class Game {
     constructor(ms, row, col) {
-        this.grid = makeLine(col).map(_ => makeLine(row));
+        this.row = col;
+        this.col = col;
+        this.grid = makeLine(row).map(_ => makeLine(col));
         this.ms = ms;
     }
 
     start() {
-        figure = new Figure(figures[random(figures.length)]);      
+        figure = new Figure(figures[random(figures.length)], Math.trunc(this.col / 2));      
 
         const timerID = setInterval(() => {           //  INTERVAL FOR THE RHYTHM OF GAME, ENSURE PIECES GO DOWN
             let bool = this.updateGrid(figure);
@@ -121,68 +115,96 @@ class Game {
     }
 
     executeAction(key) {
-        if (key === "ArrowLeft" && !Figure.checkOutsideBoard(figure, "left")) {
-            this.updateGrid(figure, "left");
-        } else if (key === "ArrowRight" && !Figure.checkOutsideBoard(figure, "right")) {
-            this.updateGrid(figure, "right");
-        } else if (key === "ArrowUp") {
-            this.updateGrid(figure, "up");
-            figure.position = !figure.position;
+        switch (key) {
+            case "ArrowLeft":
+                this.updateGrid(figure, "left");
+                break;
+            case "ArrowRight":
+                this.updateGrid(figure, "right");
+                break;
+            case "ArrowUp":
+                this.updateGrid(figure, "up");
+                break;
         }
     }
 
     // KEY FUNCTION, ENSURE RHYTHM OF GAME AND UPDATE POSITION OF PIECES, EITHER VERTICAL OR HORIZONTAL
-    updateGrid({ coords: { square1, square2, square3, square4 } }, direction) {
-        const squares = [square1, square2, square3, square4];
+    updateGrid(figure, direction) {
+        this.checkFullRow();
+        const oldCoords = deepCopy(figure.currentPosition); 
 
-        for (const square of squares) {
-            //IF COLLISION OCCUR, FIGURE IS INTEGRATED TO BOARD
-            if(this.checkCollision(square)) {
-                squares.forEach(square => this.updateCell(square, "#"));
+        switch (direction) {
+            case "right":
+                figure.moveRight();
+                break;
+            case "left":
+                figure.moveLeft();
+                break;
+            case "up":
+                figure.spin();
+                break;
+            default:
+                figure.updatePosition();
+                break;
+        }
+
+        const newCoords = deepCopy(figure.currentPosition);
+
+        if (this.checkCollision(figure)) {
+            figure.currentPosition = oldCoords;
+
+            if (!direction) { 
+                this.placeFigure(figure.currentPosition); 
+                return true; 
+            } else {
+                return false;
+            }
+        } else {
+            oldCoords.forEach(square => this.updateCell(square, "~"));
+            newCoords.forEach(square => this.updateCell(square, "*"));
+        }
+    }
+
+    checkCollision(figure) {
+        for (const square of figure.currentPosition) {
+            if (this.grid?.[square[1]]?.[square[0]] === "#" ||
+                square[1] < 0                               ||
+                square[1] > this.grid[0].length - 1         ||
+                square[0] === this.grid.length)
+            {
                 return true;
             }
         }
-    
-        squares.forEach(square => this.updateCell(square, "~"));
-        squares.forEach(square => {                                 // IMPORTANT, ALL THE ACTIONS ARE HERE
-            if (direction === "right") Figure.moveRight(square);
-            else if (direction === "left") Figure.moveLeft(square);
-            else if (direction === "up") Figure.spin(square);
-            else Figure.updatePosition(square);
-            this.updateCell(square, "*");
-        });
+        return false;
     }
 
-    checkCollisionByMove(figure, direction) {
-        const squareCopy = {};
-        Object.assign(squareCopy, square);
-
-        if (direction === "up") {
-            Figure.spin(squareCopy);
-        } else if (direction === "left") {
-            Figure.moveLeft(squareCopy);
-        } else if (direction === "right") {
-            Figure.moveRight(square);
-        }
-
-        // CHECK IF SPIN OR DISPLACEMENT IS POSSIBLE
-        if (  (this.grid?.[squareCopy.positionY]?.[squareCopy.positionX] === "#") ||
-               this.grid?.[squareCopy.positionY]?.[squareCopy.positionX] === undefined ) {
-            return;
-        }
-    }
-
-    updateCell({ positionX, positionY }, char) {
+    updateCell([ positionY, positionX ], char) {
         if (positionY < 0) return;
-        this.grid[positionY][positionX] = char;
-    }
-
-    checkCollision({ positionX, positionY }) {
-        return this.grid?.[positionY + 1]?.[positionX] === "#" || positionY + 1 === this.grid.length;
+        this.grid[positionX][positionY] = char;
     }
 
     placeFigure(arrOfSquares) {
         arrOfSquares.forEach(square => this.updateCell(square, "#"));
+    }
+
+    // REMOVE FULL ROWS
+    checkFullRow() {
+        for (let i = this.grid.length - 1; i >= 0; i--) {
+            if (this.checkRow(this.grid[i])) this.shiftAllRows(i);
+        }
+        console.log(true)
+    }
+
+    checkRow(row) {
+        return row.every(elem => elem === "#") ? true : false;
+    }
+
+    shiftAllRows(pos) {
+        for (col of this.grid) {
+            for (let row = pos; row > 0; row--) {
+                if (col[row] !== "*" && col[row - 1] !== "*") col[row] = col[row - 1];
+            }
+        }
     }
 }
 
@@ -201,18 +223,30 @@ class DOMmanipulator {
     }
 
     startGame() {
-        const rows = 15, cols = 10, ms = 500;
+        const rows = 10, cols = 10, ms = 500;
         game = new Game(ms, rows, cols);
         game.start();
     }
 
     printDOM(grid) {
-        this.gridDOM.textContent = grid.reduce((acc, row) => acc + (row.join(" ") + "\n"), "");
+        let formattedString = "";
+        const colLength = grid[0].length
+
+        for(let row = 0; row < colLength; row++) {
+
+            for(let col of grid) {
+                formattedString += col[row];
+            }
+
+            formattedString += "\n";
+        }
+
+        this.gridDOM.textContent = formattedString;
     }
 }
 
 // GLOBAL VARIABLES 
-// const DOM = new DOMmanipulator;
+const DOM = new DOMmanipulator;
 let game, figure;
 let timerKey, arrowKeyPressed;
 
